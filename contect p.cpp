5968a1a2c72493992b76fbc8f336607b3ff80c22
Ahbabsaqlan <ahbabzami3@gmail.com>
2022-06-24 21:17:04 +0600

    do fectorial

--- contect p.cpp
+++ contect p.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main(){
 
+    int num;
+    long long int factorial=1;
+    cin>>num;
+    if(num>=0 && num<=20){
+        for(int i=0;i<num;i++){
+            factorial=factorial*(i+1);
+        }
+        cout<<factorial;
+    }
 
     return 0;
 }
