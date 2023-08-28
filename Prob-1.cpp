8392a1620a789b68d3deb50c11bffb775acbaa47
Ahbabsaqlan <ahbabzami3@gmail.com>
2023-08-28 23:38:13 +0600

    .

--- Prob-1.cpp
+++ Prob-1.cpp
@@ -13,16 +13,32 @@ int main()
     cout<<"Enter Your Array text: ";
     cin.getline(arr,size+1);
     cout<<endl;
-    cout<<"Your Entered Array Text: "<<arr<<endl;
-    
 
     char revArr[size];
     for(int i=0;i<size;i++){
         revArr[i]=arr[(size-1)-i];
     }
+
+    int count=0;
+    int index=0;
+    char final[size];
+    for(int i=0;i<size;i++){
+        if(revArr[i]==' '){
+            index=i;
+            for(int j=count;j<i;j++){
+                final[j]=revArr[--index];
+                count++;
+            }
+            final[count++]=' ';
+        }
+    }
+    for(int k=count;k<size;k++){
+        final[k]=revArr[((size-1)-k)+count];
+    }
     
-    cout<<"Your Entered Array Text In Reverse: "<<revArr<<endl;
-    
-    
+    cout<<"Your Entered Array Text In Reverse: ";
+    for(int l=0;l<size;l++){
+        cout<<final[l];
+    }
 	return 0;
 }
